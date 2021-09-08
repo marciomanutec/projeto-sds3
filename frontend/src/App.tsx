fb87cca34432034381f3187b2b3f33748462d98c
marciomanutec <meushost@gmail.com>
2021-09-07 22:26:19 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div >
-      <h1 className="text-primary">Manutec de Volta...</h1>
-      <p>Retorceder nunca render-se jamais...</p>
-      <input type="checkbox" name="idteste" id="idteste" />
-      <input type="radio" name="idradio" />
-      <input type="text" name="teste" id="teste" />
-    </div>
+    < >
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Manutec de Volta...</h1>
+        <p>Retorceder nunca render-se jamais...</p>
+        <DataTable />
+      </div>
+      <Footer />
+
+    </ >
   );
 }
 
