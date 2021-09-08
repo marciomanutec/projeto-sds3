0c279a25d07b1168258947f1a876d06254b12737
marciomanutec <meushost@gmail.com>
2021-09-07 21:34:13 -0300

    Add Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
 function App() {
   return (
     <div >
-      <h1>Manutec de Volta...</h1>
+      <h1 className="text-primary">Manutec de Volta...</h1>
       <p>Retorceder nunca render-se jamais...</p>
+      <input type="checkbox" name="idteste" id="idteste" />
+      <input type="radio" name="idradio" />
+      <input type="text" name="teste" id="teste" />
     </div>
   );
 }
