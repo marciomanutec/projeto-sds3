feec065a67b76818effa6d03212ad70332e3698f
marciomanutec <meushost@gmail.com>
2021-11-04 23:22:23 -0300

    correcao donutchart

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,9 +1,4 @@
-import axios from 'axios';
-import { useState } from 'react';
 import Chart from 'react-apexcharts';
-import { Fornecedores } from 'types/fornecedor';
-import { SaleSum } from 'types/sale';
-import { BASE_URL } from 'utils/requests';
 
 type ChartData = {
     labels: string[],
@@ -12,32 +7,8 @@ type ChartData = {
 
 const DonutChart = () => {
 
-    //Forma certa
-  //  const [chartData, setChartData] = useState<ChartData>({ labels: ['Cudinha'], series: [852088] });
-
-    // Forma errada
-    // let chartData: ChartData = { labels: ['Cudinha'], series: [85200.20] };
 
 
-
-    // forma errada
-    axios.get(`${BASE_URL}search/shows?q=star%20wars`)
-        .then(response => {
-            const data = response.data as SaleSum[]; // cast
-            const datafornec = response.data as Fornecedores[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
-           // const fornecNome = datafornec.map(f => f.fornecedor);
-
-            //  chartData = { labels: ['Gilmar'], series: [100000] };
-        //    console.log(chartData.labels);
-        //    console.log(chartData.series);
-            //  setChartData({ labels: ['Gilmar'], series: [120000] })
-
-            //  console.log(response.data)
-
-
-        })
 
     const mockData = {
         series: [477138, 499928, 444867, 220426, 473088, 852088],
@@ -51,8 +22,6 @@ const DonutChart = () => {
     }
     return (
         <Chart
-            //   options={{ ...options, labels: mockData.labels }}
-            //   series={mockData.series}
             options={{ ...options, labels: mockData.labels }}
             series={mockData.series}
             type="donut"
